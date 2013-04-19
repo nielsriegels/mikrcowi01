bc7387d733bea78f6c2fb505228e672b119cef79
mikkelakromann <mikr@cowi.dk>
2013-04-19 10:52:30 +0200

    Added transmission of electricity and unserved demand, without data. Changes to results.

--- solutions/renameS.cmd
+++ solutions/renameS.cmd
@@ -1,1 +1,1 @@
-copy /Y  c:\mikr\mikrcowi01\solutions\solution.gdx c:\mikr\mikrcowi01\solutions\solution-s06.gdx
+copy /Y c:\mikr\mikrcowi01\solutions\solution-s02.gdx c:\mikr\mikrcowi01\solutions\solution.gdx
