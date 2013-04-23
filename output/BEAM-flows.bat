6952685418d3ba0c4e958c5151f63d2ce8612180
mikkelakromann <mikr@cowi.dk>
2013-04-23 23:39:31 +0200

    Creation of energy scenarions and correction of bug which prevented RoR generation

---
+++ output/BEAM-flows.bat
@@ -0,0 +1,1 @@
+"c:\Program Files\GAMS21.2-old\gdxxrw.exe" I=BEAM-flows.gdx     O=BEAM-flows.xls     par=rFlow  rng=rFlow!B10 par=rsFlow rdim=4 rng=rsFlow!B10 rdim=4 par=rcFlow rdim=4 rng=rcFlow!B10 rdim=4 runMacros=3
--- output/flows.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files\GAMS21.2-old\gdxxrw.exe" I=BEAM-agri.gdx     O=BEAM-agri.xls     par=rAgri  rng=rAgri!E7   rdim=3       runMacros=3
