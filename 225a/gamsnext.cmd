e82abd93ae5c3e4b960f2eee0cfae28076499148
mikkelakromann <mikr@cowi.dk>
2013-04-28 11:07:48 +0200

    Updates to scenarios and output

---
+++ 225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsco3nt.cmd "c:\mikr\mikrcowi01\225a\" "c:\mikr\mikrcowi01\" %1 "c:\mikr\mikrcowi01\225a\gamscntr.scr" "C:\Program Files\GAMS21.2-old\"
---
+++ output/BEAM-elec.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\GAMS21.2-old\gdxxrw.exe" I=BEAM-elec.gdx     O=BEAM-elec.xls     par=rcElec rng=rcElec!l10 rDim=3 runMacros=3
+pause
