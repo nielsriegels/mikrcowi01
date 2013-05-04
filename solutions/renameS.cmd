953e3b359796eb9f9c0292ddd7901149bfbfe90e
mikkelakromann <mikr@cowi.dk>
2013-05-04 17:28:57 +0200

    Fixed problems with CMX and land use, and YWR. Agri seems to work well now

--- solutions/renameS.cmd
+++ solutions/renameS.cmd
@@ -1,1 +1,1 @@
-copy /Y  c:\mikr\mikrcowi01\solutions\solution.gdx c:\mikr\mikrcowi01\solutions\solution-s41.gdx
+copy /Y  c:\mikr\mikrcowi01\solutions\solution.gdx c:\mikr\mikrcowi01\solutions\solution-s43.gdx
