c3931de4d82fa955d41abe15acc4c94f21495045
Mikkel Eliasen <mkel@cowi.dk>
2013-05-07 15:58:17 +0200

    Added rRbView and rSView.

--- output/BEAM-details.bat
+++ output/BEAM-details.bat
@@ -1,2 +1,2 @@
-"c:\Program Files\GAMS21.2-old\gdxxrw.exe" I=BEAM-details.gdx O=BEAM-details.xls Index=GDXindex!b6
+"c:\Program Files\GAMS21.2\gdxxrw.exe" I=BEAM-details.gdx O=BEAM-details.xls Index=GDXindex!b6
 pause
